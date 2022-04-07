8f4e34a6ce11259f3d0d148ea02b4d9840a51bad
mars <mars@st.com>
2022-04-07 00:36:57 +0000

    laravel/ui bootstrap--npm run dev 有个小问题， 小屏时，登录注册变成下拉，但无法弹出菜单

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,22 +4,31 @@
       <a class="navbar-brand " href="{{ url('/') }}">
         LaraBBS
       </a>
+
+
+      {{-- 本段代码有故障 --}}
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
   
-      <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <!-- Left Side Of Navbar -->
         <ul class="navbar-nav">
   
         </ul>
   
         <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav navbar-right">
+        <ul class="navbar-nav navbar-end">
           <!-- Authentication Links -->
           <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
           <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
         </ul>
       </div>
+
+
+
+
+
+
     </div>
   </nav>
